251f0f3a9eba681f1f6942b0cafae25b3644e7f3
Jean-Christophe Sicotte-Brisson <42256489+jcsb1994@users.noreply.github.com>
2021-10-07 17:51:59 -0400

    placement, QFN package

---
+++ Anahata_firmware.cydsn/main.c
@@ -0,0 +1,51 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    EZI2C_Start();  // Tune capsense sensors with I2C
+
+    EZI2C_EzI2CSetBuffer1(
+                      sizeof(CapSense_dsRam),
+                      sizeof(CapSense_dsRam),
+                      (uint8 *) &CapSense_dsRam 
+                     );
+    
+    CapSense_Start();
+    
+    CapSense_ScanAllWidgets();
+    
+    for(int i = 0; i < 5; i++)
+    {
+        
+        ledPin_Write(1);
+        CyDelay(100);
+        
+        ledPin_Write(0);
+        CyDelay(100);
+    }
+    
+    for(;;)
+    {
+        if(CapSense_IsBusy() == CapSense_NOT_BUSY)
+        {
+            CapSense_RunTuner();
+            CapSense_ScanAllWidgets();
+        }
+    }
+}
+
+/* [] END OF FILE */
